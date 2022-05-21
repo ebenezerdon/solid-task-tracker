20db6e9f54561afa9593b784002d441bd9b8d6f8
ebenezerdon <ebenezerdonu@gmail.com>
2022-05-21 21:30:56 +0100

    setup app template

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,40 @@
-import type { Component } from 'solid-js';
-
-import logo from './logo.svg';
-import styles from './App.module.css';
+import type { Component } from 'solid-js'
 
 const App: Component = () => {
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
-    </div>
-  );
-};
+    <div class="container mt-5 text-center">
+      <h1 class="mb-4">Whattodo!</h1>
 
-export default App;
+      <form class="mb-5 row row-cols-2 justify-content-center">
+        <input type="text" class="input-group-text p-1 w-25" placeholder="Add task here..." required />
+
+        <button class="btn btn-primary ms-3 w-auto" type="submit">
+          Add task
+        </button>
+      </form>
+
+      <div>
+        <h4 class="text-muted mb-4">Tasks</h4>
+        <div class="row row-cols-3 mb-3 justify-content-center">
+          <button class="btn btn-danger w-auto">X</button>
+          <div class="bg-light p-2 mx-2">Publish video tutorial</div>
+          <input type="checkbox" role="button" class="form-check-input h-auto px-3" />
+        </div>
+
+        <div class="row row-cols-3 mb-3 justify-content-center">
+          <button class="btn btn-danger w-auto">X</button>
+          <div class="bg-light p-2 mx-2">Write technical article</div>
+          <input type="checkbox" role="button" class="form-check-input h-auto px-3" />
+        </div>
+
+        <div class="row row-cols-3 mb-3 justify-content-center">
+          <button class="btn btn-danger w-auto">X</button>
+          <div class="bg-light p-2 mx-2">Push code to GitHub</div>
+          <input type="checkbox" role="button" class="form-check-input h-auto px-3" />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,5 @@
-/* @refresh reload */
-import { render } from 'solid-js/web';
+import { render } from 'solid-js/web'
+import App from './App'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-import './index.css';
-import App from './App';
-
-render(() => <App />, document.getElementById('root') as HTMLElement);
+render(() => <App />, document.getElementById('root') as HTMLElement)
